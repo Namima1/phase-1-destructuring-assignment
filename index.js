615bcca1392afdb4bbcdc0a250025f5e39706f01
Namima Islam <namimaislam308@gmail.com>
2024-07-13 15:07:05 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
 const farmAnimals = 'cow horse sheep pig chicken';
+const [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+console.log(moo, neigh, baa, oink, cluck);
+
+// 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
+const newFarmAnimals = farmAnimals.split(" ");
+newFarmAnimals.splice(1, 1);
+const [bessie, dolly, babe, little] = newFarmAnimals;
+
+// 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
+
+newFarmAnimals.pop;
+const [blackAndWhite, black, pink] = newFarmAnimals;
+
+// 4. Use destructuring to assign appropriate variables using the color names.
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+
+// 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
+
+const [r, o, y, g, b, , v] = colors;
+
+//6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg.
+
+const indg = colors[5];
+
+
+// 7. Use destructuring to assign all variables using the keys as the variable names
+
+
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -9,6 +38,10 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+
+const { muppetName, color, song, job, partner } = muppet; 
+
+// 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,6 +57,23 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+
+const {
+  album: {
+    theMuppetMovie: { song2, song4 }
+  },
+  nestedJob, 
+  nestedPartner
+} = nestedMuppet;
+
+console.log(song2);
+console.log(song4);
+console.log(nestedJob);
+console.log(nestedPartner);
+
+
+
+
 
 // Strings
 
